93faf2968ef6155ef3307a798990c72c138c5453
lucasrisseto2508 <129887595+lucasrisseto2508@users.noreply.github.com>
2023-04-07 13:39:12 -0300

    Add files via upload

---
+++ destino turistico/app.js
@@ -0,0 +1,32 @@
+new fullScroll({
+    mainElement: "main",
+    displayDots: true,
+    dotsPosition: "left",
+    animateTime: 0.7,
+    animateFunction: "ease",
+});
+
+const navSlide = () =>  {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.nav-links');
+    const linha1 = document.querySelector('.linha1');
+    const linha2 = document.querySelector('.linha2');
+    const linha3 = document.querySelector('.linha3');
+    const links = document.querySelectorAll('.nav-links li');
+ 
+
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('nav-active');
+        linha1.classList.toggle('linha1-active');
+        linha2.classList.toggle('linha2-active');
+        linha3.classList.toggle('linha3-active');
+
+        links.forEach(link => {
+            link.classList.toggle('active');
+        });
+
+
+    });
+}
+
+navSlide();
